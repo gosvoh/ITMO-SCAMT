36e7e4798a65f242fedb25b127f6f8eb37a899cc
Aleksey Vokhmin <vohmina2011@yandex.ru>
2022-02-05 02:31:05 +0300

    Fixed difficulty choosing

--- Assets/ITMO/Scripts/Difficulty.cs
+++ Assets/ITMO/Scripts/Difficulty.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace ITMO.Scripts
@@ -7,35 +6,14 @@ namespace ITMO.Scripts
     public class Difficulty : MonoBehaviour
     {
         [SerializeField] private Server server;
-        
+
         public void ChooseDifficultyAndRun()
         {
             var dropdown = GameObject.Find("Dropdown").GetComponent<Dropdown>();
-            switch (dropdown.value)
-            {
-                case 0:
-                    server.Send(Level.GetLevelPath(Level.Levels["1"].First.Value));
-                    Level.CurrentLevelName = Level.Levels["1"].First.Value;
-                    server.Connect();
-                    break;
-                case 1:
-                    server.Send(Level.GetLevelPath(Level.Levels["2"].First.Value));
-                    Level.CurrentLevelName = Level.Levels["2"].First.Value;
-                    server.Connect();
-                    break;
-                case 2:
-                    server.Send(Level.GetLevelPath(Level.Levels["3"].First.Value));
-                    Level.CurrentLevelName = Level.Levels["3"].First.Value;
-                    server.Connect();
-                    break;
-                case 3:
-                    server.Send(Level.GetLevelPath(Level.Levels["4"].First.Value));
-                    Level.CurrentLevelName = Level.Levels["4"].First.Value;
-                    server.Connect();
-                    break;
-                default:
-                    throw new NullReferenceException("Неверный выбор уровня сложности!");
-            }
+            var difficulty = dropdown.options[dropdown.value].text;
+            Server.Send(Level.GetLevelPath(Level.DifficultyLevels[difficulty].First.Value));
+            Level.CurrentLevelName = Level.DifficultyLevels[difficulty].First.Value;
+            server.Connect();
         }
     }
 }
