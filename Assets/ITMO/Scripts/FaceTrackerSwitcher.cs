30f61ff369f3a829848cd38958afc5f5ca56cb89
Aleksey Vokhmin <vohmina2011@yandex.ru>
2022-04-10 14:09:20 +0300

    Moved assets

---
+++ Assets/ITMO/Scripts/FaceTrackerSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Tobii.XR;
+using UnityEngine;
+using ViveSR.anipal.Eye;
+
+namespace ITMO.Scripts
+{
+    public class FaceTrackerSwitcher : MonoBehaviour
+    {
+        public static bool TobiiEnabled = false;
+
+        private void FixedUpdate()
+        {
+            if (TobiiEnabled) EnableTobii();
+            else DisableTobii();
+        }
+
+        private void DisableTobii()
+        {
+            TobiiXR.Stop();
+            SRanipal_Eye_Framework.Instance.StartFramework();
+            Debug.Log("Tobii disabled");
+        }
+
+        private void EnableTobii()
+        {
+            SRanipal_Eye_Framework.Instance.StopFramework();
+            TobiiXR.Start(new TobiiXR_Settings(EyeInteraction.PrefabLayer));
+            Debug.Log("Tobii enabled");
+        }
+    }
+}
