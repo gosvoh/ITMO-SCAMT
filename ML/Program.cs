fadf8a36de887ab2bfd9333c31801849bc42d60d
Aleksey Vokhmin <vohmina2011@yandex.ru>
2022-04-18 18:18:03 +0300

    Add project files.

---
+++ ML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleApp.ModelBuilder.CreateModel("Path to .csv file for training model", "Path to .zip model for saving model");
+        }
+    }
+}
